4da03582f7c969bc99e3833fa08cd1222cf459b4
troolytrap <kevinmcdermott@uchicago.edu>
2025-03-14 14:48:11 +0000

    current

---
+++ app/controllers/operations_controller.rb
@@ -0,0 +1,35 @@
+class OperationsController < ApplicationsController
+
+  def square
+    render({ :template => "operations/square" })
+  end
+  def sqrt
+    render({ :template => "operations/sqrt" })
+  end
+  def payment
+    render({ :template => "operations/payment" })
+  end
+
+  def random
+    render({ :template => "operations/random" })
+  end
+
+  def square_results
+    render({ :template => "results/square" })
+  end
+  def sqrt_results
+    render({ :template => "results/sqrt" })
+  end
+  def payment_results
+    @apr = params.fetch("apr").to_f
+   @numberofyears = params.fetch("numberofyears").to_f
+   @principal = params.fetch("principal").to_f
+    render({ :template => "results/payment" })
+  end
+
+  def random_results
+    @randmin = params.fetch("user_min").to_f
+    @randmax = params.fetch("user_max").to_f
+
+    render({ :template => "results/random" })
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller => operations, :action => square})
+  get("/square/new", {:controller => operations, :action => square})
+  get("/square/results", {:controller => operations, :action => squareresults})
+  get("/square_root/new", {:controller => operations, :action => sqrt})
+  get("/square_root/results", {:controller => operations, :action => sqrtresults})
+  get("/payment/new", {:controller => operations, :action => payment})
+  get("/payment/results", {:controller => operations, :action => paymentresults})
+  get("/random/new", {:controller => operations, :action => random})
+  get("/random/results", {:controller => operations, :action => randomresults})
 end
